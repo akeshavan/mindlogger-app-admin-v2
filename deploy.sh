51b4ed9996500cb440942d037a3d86705e64da15
Anisha Keshavan <anisha@childmind.org>
2019-04-26 11:18:49 -0700

    enh: travis deploy for v2

--- deploy.sh
+++ deploy.sh
@@ -34,6 +34,55 @@ git push -fq origin gh-pages > /dev/null
 
 echo "Done updating gh-pages\n"
 
+elif [[ $TRAVIS_BRANCH != "gh-pages" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+
+echo "Starting to update gh-pages from feature branch\n"
+pwd
+
+ #copy data we're interested in to other place
+ cp -R dist $HOME/dist
+
+
+ #go to home and setup git
+ cd $HOME
+ git config --global user.email "travis@travis-ci.org"
+ git config --global user.name "Travis"
+
+echo "setup git\n"
+pwd
+
+ #create a folder named after the branch
+ git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+ pwd
+ echo "going to check ls \n"
+ ls
+ echo "\n"
+
+#go into directory and copy data we're interested in to that directory
+cd gh-pages
+echo "see that we are in the gh-pages branch\n\n"
+git branch -v
+echo "\n\ncreating a directory\n\n"
+mkdir -p $TRAVIS_BRANCH
+pwd
+ cd $TRAVIS_BRANCH
+ cp -Rf $HOME/dist/* .
+
+echo "printing the current working dir"
+pwd
+
+ echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
+ echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
+
+ #add, commit and push files
+ git add -f .
+ git status
+ git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+ git branch -v
+ git push -fq origin gh-pages > /dev/null
+
+ echo "Done updating gh-pages with build triggered from feature branch ${TRAVIS_BRANCH}\n"
+
 else
- echo "Skipped updating gh-pages, because build is not triggered from the master branch."
+ echo "Skipped updating gh-pages, because build is not triggered from the master or feature branch."
 fi;
